2b859b8feb61e49db15a99d64fc6d47f7234bc5a
Yasir Mehboob <jdbsflhr17>
2021-01-26 18:48:24 +0500

    commited

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import './App.css';
 import Login from "./component/login/login";
 import SingUp from './component/singup/singup';
 import NavBar from './component/layout/navbar';
+import { Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <SingUp />
+      <BrowserRouter>
+        <NavBar />
+        <Route path="/" exact component={Login} />
+        <Route path="/singup" component={SingUp} />
+      </BrowserRouter>
     </div>
   );
 }
